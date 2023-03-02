3c1d2828be5780917a7b0d2f2f9c86e8e66ed367
Arjav Sethi <arjavsethi07@gmail.com>
2023-03-02 13:15:37 +0530

    added a circle on input tag and when the circle is clicked a pop up appears

--- foreground.js
+++ foreground.js
@@ -1,7 +1,36 @@
-// This script gets injected into any opened page
-// whose URL matches the pattern defined in the manifest
-// (see "content_script" key).
-// Several foreground scripts can be declared
-// and injected into the same or different pages.
+// create the image element
+const img = document.createElement("div");
+img.src = "";
+img.alt = "";
+img.id = "messageOpenai";
+img.style.cursor = "pointer"; // make the cursor change on hover
+img.style.position = "absolute";
+img.style.top = "15px";
+img.style.left = "15px";
+img.style.minWidth = "10px";
+img.style.minHeight =  "10px";
+img.style.borderRadius =  "50%";
+img.style.background= "red";
+img.addEventListener("click", myFunction); // attach the click event listener
 
-console.log("This prints to the console of the page (injected only if the page url matched)")
+
+
+// the function to be called on image click
+function myFunction() {
+  // do something
+  console.log("clicked")
+  popupContainer.style.display = 'block';
+}
+
+// Using set-timeout need to find better solution 
+setTimeout(function() {
+    // Code to be executed after 3 seconds
+    const parentDiv = document.querySelector('.msg-form__msg-content-container--scrollable.scrollable.relative');
+
+    // insert the image element inside the parent div element
+    if (parentDiv) {
+      parentDiv.appendChild(img);
+    } else {
+      console.log("Parent element not found.");
+    }
+  }, 3000);
---
+++ msgPopu.js
@@ -0,0 +1,69 @@
+console.log("Hello")
+// Create popup container
+const popupContainer = document.createElement('div');
+popupContainer.setAttribute('id', 'popup-container');
+popupContainer.style.display = 'none';
+popupContainer.style.position = 'fixed';
+popupContainer.style.top = '0';
+popupContainer.style.left = '0';
+popupContainer.style.width = '100%';
+popupContainer.style.height = '100%';
+// popupContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+popupContainer.style.zIndex = '9999';
+
+// Create popup content
+const popup = document.createElement('div');
+popup.setAttribute('class', 'popup');
+popup.style.position = 'absolute';
+popup.style.top = '50%';
+popup.style.left = '40%';
+popup.style.transform = 'translate(-50%, -50%)';
+popup.style.backgroundColor = 'white';
+popup.style.padding = '20px';
+popup.style.borderRadius = '10px';
+popup.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+popup.style.textAlign = 'center';
+popup.style.width = '300px';
+popup.style.height = '200px';
+popup.style.fontSize = '20px';
+popup.style.color = '#333';
+
+// Create close button
+const closeButton = document.createElement('span');
+closeButton.setAttribute('class', 'close');
+closeButton.style.position = 'absolute';
+closeButton.style.top = '10px';
+closeButton.style.right = '10px';
+closeButton.style.fontSize = '20px';
+closeButton.style.color = '#333';
+closeButton.style.cursor = 'pointer';
+closeButton.innerHTML = '&times;';
+
+// Add close button to popup
+popup.appendChild(closeButton);
+
+// Create popup content
+const popupContent = document.createElement('div');
+popupContent.innerHTML = '<h6>Generate Message</h6><hr/><div class = "messageList"></div>';
+
+// Add popup content to popup
+popup.appendChild(popupContent);
+
+// Add popup to container
+popupContainer.appendChild(popup);
+
+// Add popup container to document
+document.body.appendChild(popupContainer);
+
+
+// Get references to DOM elements
+const openButton = document.getElementById('messageOpenai');
+
+
+
+
+
+// Add event listener to close button
+closeButton.addEventListener('click', () => {
+  popupContainer.style.display = 'none';
+});
